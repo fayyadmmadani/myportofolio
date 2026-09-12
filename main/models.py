99002f19528f4f480fdb5497fd4619dce8cc425d
Fayyad M Madani <fayyadmadani.11@gmail.com>
2026-09-12 19:37:58 +0700

    feat: projects page - model - migration - view, template, route - tests

--- main/models.py
+++ main/models.py
@@ -25,3 +25,12 @@ class Experience(models.Model):
     def is_ongoing(self):
         return self.ended_at is None
     
+class Project(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    thumbnail = models.URLField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.title
+    
--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from main.models import Experience
+from main.models import Experience, Project
 
 
 def show_main(request):
@@ -24,3 +24,11 @@ def show_experience(request):
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
+
+
+def show_projects(request):
+    context = {
+        "name": "Fayyad Mohammad Madani",
+        "project_list": Project.objects.all(),
+    }
+    return render(request, "projects.html", context)
