6615f523fdb1760e749e54d954d78fd6319434b9
Fayyad M Madani <fayyadmadani.11@gmail.com>
2026-09-16 17:12:22 +0700

    fix: merge conflict

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 
-from main.views import show_main, show_experience, show_projects
+from main.views import show_main, show_experience, get_experience_json, create_experience, edit_experience, delete_experience, show_projects, get_projects_json, create_project, delete_project
 
 app_name = "main"
 
 urlpatterns = [
     path("", show_main, name="show_main"),
+    
     path("experience/", show_experience, name="show_experience"),
+    path("api/experience/", get_experience_json, name="get_experience_json"),
+    path("experience/add/", create_experience, name="create_experience"),
+    path("experience/<uuid:experience_id>/edit/", edit_experience, name="edit_experience"),
+    path("experience/<uuid:experience_id>/delete/", delete_experience, name="delete_experience"),
+    
     path("projects/", show_projects, name="show_projects"),
 ]
--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.db.models import F
 from django.shortcuts import render
 
 from main.models import Experience, Project
+from main.forms import ProjectForm, ExperienceForm
 
 
 def show_main(request):
@@ -24,14 +25,70 @@ def show_main(request):
 
 
 def show_experience(request):
+    json_response = get_experience_json(request)
+
+    experiences = serializers.deserialize(
+        "json",
+        json_response.content.decode("utf-8"),
+    )
+    experiences = [experience.object for experience in experiences]
+
     context = {
         "name": "Fayyad Mohammad Madani",
-        "experience_list": Experience.objects.all().order_by(
-            F("ended_at").desc(nulls_first=True), "-started_at"
-        ),
+        "experience_list": experiences,
     }
     return render(request, "experience.html", context)
 
+def get_experience_json(request):
+    category_query = request.GET.get("category", "").strip()
+    experiences = Experience.objects.all().order_by(
+        F("ended_at").desc(nulls_first=True), "-started_at"
+    )
+    
+    if category_query:
+        experiences = experiences.filter(category=category_query)
+    
+    experience_json = serializers.serialize("json", experiences)
+    return HttpResponse(experience_json, content_type="application/json")
+
+def create_experience(request):
+    form = ExperienceForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        messages.success(request, "Experience baru berhasil ditambahkan!")
+        return redirect("main:show_experience")
+
+    context = {
+        "name": "Fayyad Mohammad Madani",
+        "form": form,
+    }
+    return render(request, "experience_form.html", context)
+
+def edit_experience(request, experience_id):
+    experience = get_object_or_404(Experience, pk=experience_id)
+    form = ExperienceForm(request.POST or None, instance=experience)
+
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        messages.success(request, "Experience berhasil diperbarui!")
+        return redirect("main:show_experience")
+
+    context = {
+        "name": "Fayyad Mohammad Madani",
+        "form": form,
+    }
+    return render(request, "experience_form.html", context)
+
+def delete_experience(request, experience_id):
+    experience = get_object_or_404(Experience, pk=experience_id)
+
+    if request.method == "POST":
+        experience.delete()
+        messages.success(request, "Experience berhasil dihapus!")
+        return redirect("main:show_experience")
+
+    return redirect("main:show_experience")
 
 def show_projects(request):
     context = {
